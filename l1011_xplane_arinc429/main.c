88bb389745db790b5be88a078c39dc1ba313db34
Curd Zechmeister <curdzechmeister@gmail.com>
2014-06-14 22:03:54 -0400

    Initial Commit

---
+++ l1011_xplane_arinc429/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  l1011_xplane_arinc429
+//
+//  Created by Curd Zechmeister on 6/14/14.
+//  Copyright (c) 2014 Curd Zechmeister. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
